75de175e784a23aa7882253a078f417870227152
simon <simon@dancingcloudservices.com>
2022-02-04 12:13:56 -0700

    Initial

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "SafariScala"
+
+version := "0.1"
+
+scalaVersion := "3.1.1"
---
+++ src/main/scala/verify/Verify.scala
@@ -0,0 +1,13 @@
+package verify
+
+object Verify extends App:
+  print("Hello Scala3 application")
+
+object V2:
+  def main(args:Array[String]):Unit =
+    Console print "Hello Scala3 world!"
+
+object V3 {
+  def main(args:Array[String]):Unit =
+    print("Hello Scala2")
+}
